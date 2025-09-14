2aa310554cd8c1a46ee7bac19ed326cf36682ed1
Syrgak Sulaimanov <delcatessen@gmail.com>
2025-09-14 15:38:34 +0600

    added backend url

--- gamestore/backend/src/server.ts
+++ gamestore/backend/src/server.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: `https://framelane-2.onrender.com/`,
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"],
     })
